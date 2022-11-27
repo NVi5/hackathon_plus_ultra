61673ff9b3ae7c7f433c3f8f98d2641365819f4f
Jan Wołowiec <wolowiec.jan7@gmail.com>
2022-11-27 08:54:40 +0100

    reszta

--- src/frame_assembly.v
+++ src/frame_assembly.v
@@ -86,6 +86,8 @@ module frame_assembly (
     output reg  [7:0]       o_wdata,
     output reg              o_wvalid,
 
+    input  wire [15:0]      i_scs,
+
     input  wire [15:0]      i_dst,
     input  wire [15:0]      i_src,
     input  wire [15:0]      i_size,
@@ -99,7 +101,7 @@ module frame_assembly (
     input  wire             start
 );
 
-reg [15:0] scs;
+// reg [15:0] scs;
 
 localparam MHP_FRAME_LEN    = 51;
 
@@ -120,32 +122,33 @@ always @(posedge clk) begin
         done <= 1'b0;
         ctr <= 0;
         shift <= 0;
-        scs <= 0;
+        // scs <= 0;
     end else begin
         case (state)
             IDLE: begin
                 ctr <= MHP_FRAME_LEN-1-(42-i_payload_size);
                 done <= 1'b0;
                 shift <= shift + 1;
-                     scs <= 0;
+                    //  scs <= 0;
                 if (start) begin
-                    frame <= {scs, i_payload, i_dir, i_type, i_size[7:0], i_size[15:8], i_src, i_dst} >> 8;
+                    frame <= {i_scs[7:0], i_scs[15:8], i_payload, i_dir, i_type, i_size[7:0], i_size[15:8], i_src[15:8], i_src[7:0], i_dst[15:8], i_dst[7:0]} >> 8;
                     o_wvalid <= 1'b1;
                     state <= FRAME_SENDING;
                     o_wdata <= i_dst[7:0];
-                    scs <= scs + (i_dst[7:0] << shift);
+                    // scs <= scs + (i_dst[7:0] << shift);
                 end
             end
             FRAME_SENDING: begin
-                if (ctr < 3 && ctr > 0) begin
-                    o_wdata <= 0;
-                    // o_wdata <= scs[15:8];
-                    // scs <= scs << 8;
-                    ctr <= ctr - 1;
-                end
-                else if (ctr > 0) begin
+                // if (ctr < 3 && ctr > 0) begin
+                //     o_wdata <= 0;
+                //     // o_wdata <= scs[15:8];
+                //     // scs <= scs << 8;
+                //     ctr <= ctr - 1;
+                // end
+                // else
+                if (ctr > 0) begin
                     shift <= shift + 1;
-                    scs <= scs + (frame[7:0] << shift);
+                    // scs <= scs + (frame[7:0] << shift);
                     state <= FRAME_SENDING;
                     o_wdata <= frame[7:0];
                     frame <= frame >> 8;
@@ -160,7 +163,7 @@ always @(posedge clk) begin
             end
             FRAME_SENT: begin
                 shift <= 0;
-                scs <= 0;
+                // scs <= 0;
                 state <= IDLE;
                 done <= 1'b0;
             end
--- src/mhp.v
+++ src/mhp.v
@@ -1,3 +1,4 @@
+`default_nettype none
 `timescale 1ns/1ns
 
 module mhp(
@@ -20,8 +21,9 @@ wire  [7:0]      o_wdata2;
 wire             o_wvalid2;
 
 
-reg [15:0]      our_dst = 16'hffff;
-reg [15:0]      our_src = 16'h0000;
+reg [15:0]      our_scs;
+reg [15:0]      our_dst;
+reg [15:0]      our_src;
 wire [15:0]      i_size = 37;
 wire             i_dir  = 1;
 reg [6:0]           our_type = 7'h3;
@@ -37,15 +39,39 @@ frame_assembly frame_assembly_i(
     .rst(i_rst),
     .o_wdata(o_wdata2),
     .o_wvalid(o_wvalid2),
+    .i_scs(our_scs),
     .i_dst(our_dst),
     .i_src(our_src),
     .i_size(i_size),
     .i_dir(i_dir),
     .i_type(our_type),
-    .i_payload(i_payload),
+    .i_payload(our_payload),
     .i_payload_size(i_payload_size),
     .done(frame_assembly_done),
     .start(frame_assembly_start)
+);
+
+
+wire [15:0]      o_dst;
+wire [15:0]      o_src;
+wire [15:0]      o_size;
+wire             o_dir;
+wire [6:0]       o_type;
+wire [335:0]     o_payload;
+// wire             o_wvalid;
+
+frame_decoder frame_decoder_i(
+    .clk(i_clk),
+    .rst(i_rst),
+    .i_rdata(i_rdata),
+    .i_rvalid(r_req),
+    .o_dst(o_dst),
+    .o_src(o_src),
+    .o_size(o_size),
+    .o_dir(o_dir),
+    .o_type(o_type),
+    .o_payload(o_payload),
+    .o_wvalid()
 );
 
 //  fsm
@@ -55,11 +81,20 @@ localparam  PING_READ        = 1;
 localparam  PING_WRITE       = 2;
 localparam  PING_LINK       = 3;
 
-reg   [1:0] mhp_state       = 0;
+reg   [3:0] mhp_state       = 0;
 localparam  MHP_ADDR_REQ    = 0;
 localparam  MHP_IDLE        = 1;
 localparam  MHP_READ        = 2;
-localparam  MHP_WRITE       = 3;
+localparam  MHP_DELAY       = 3;
+localparam  MHP_READY       = 4;
+localparam  MHP_READY_ACK1  = 5;
+localparam  MHP_READY_ACK2  = 6;
+localparam  MHP_TASK1  = 7;
+localparam  MHP_TASK2  = 8;
+localparam  MHP_PING  = 9;
+localparam  MHP_ADD_R1  = 10;
+localparam  MHP_ADD_R2  = 11;
+localparam  MHP_ADD_S  = 12;
 
 //  local regs
 reg           done      = 0;
@@ -72,17 +107,17 @@ reg           w_valid   = 0;
 reg           link;
 
 vio u0 (
-    .source (trigger_send),
+    .source (stop),
     .probe  (read_ctr)
 );
 
-wire trigger_send;
+wire stop;
 reg [7:0] read_ctr;
 
 reg [9:0] link_delay;
 
 always @(posedge i_clk) begin
-    if (i_rst) begin
+    if (i_rst || stop) begin
         done    <= 0;
         w_data  <= 0;
         w_valid <= 0;
@@ -92,6 +127,7 @@ always @(posedge i_clk) begin
         i_payload_size <= 0;
         read_ctr <= 8'h0;
         link_delay <= 500;
+        our_scs <= 16'h0000;
         our_src <= 16'h0000;
         our_dst <= 16'hffff;
         our_type <= 7'h3;
@@ -144,6 +180,11 @@ always @(posedge i_clk) begin
             case (mhp_state)
 
                 MHP_ADDR_REQ: begin
+                    our_scs <= 16'h0000;
+                    our_src <= 16'h0000;
+                    our_dst <= 16'hffff;
+                    our_payload <= 336'b0;
+                    our_type <= 7'h3;
                     i_payload_size <= 37;
                     if (!frame_assembly_done) begin
                         frame_assembly_start  <= 1'b1;
@@ -155,7 +196,7 @@ always @(posedge i_clk) begin
                 end
 
                 MHP_IDLE: begin
-                    if (i_rready || trigger_send) begin // received frame's payload ready
+                    if (i_rready) begin // received frame's payload ready
                         r_req   <= 1;     // r_req set before read state, so we can expect valid data in READ state
                         mhp_state   <= MHP_READ;
                         read_ctr <= read_ctr + 1;
@@ -169,22 +210,122 @@ always @(posedge i_clk) begin
                         r_req   <= 1;
                     else begin
                         r_req   <= 0;
-                        mhp_state   <= MHP_WRITE;
+                        mhp_state   <= MHP_READY;
+                        our_src <= o_dst;
+                        our_dst <= o_src;
+                    end
+                end
+
+                MHP_READY: begin
+                    our_scs <= 16'h0000;
+                    our_type <= 7'h12;
+                    our_payload[7:0] <= 8'h02;
+                    our_payload[15:8] <= 8'h20;
+                    our_payload[23:16] <= 8'h60;
+                    i_payload_size <= 37;
+                    if (!frame_assembly_done) begin
+                        frame_assembly_start  <= 1'b1;
+                    end
+                    else begin
+                        mhp_state   <= MHP_READY_ACK1;
+                        frame_assembly_start <= 1'b0;
                         link_delay <= 500;
                     end
                 end
 
-                MHP_WRITE: begin    //  write data
+                MHP_READY_ACK1: begin
+                    if (i_rready) begin // received frame's payload ready
+                        r_req   <= 1;     // r_req set before read state, so we can expect valid data in READ state
+                        mhp_state   <= MHP_READY_ACK2;
+                    end else begin
+                        r_req   <= 0;
+                    end
+                end
+
+                MHP_READY_ACK2: begin
+                    if (i_rready) // clear fifo
+                        r_req   <= 1;
+                    else begin
+                        r_req   <= 0;
+                        mhp_state   <= MHP_TASK1;
+                    end
+                end
+
+                MHP_TASK1: begin
+                    if (i_rready) begin // received frame's payload ready
+                        r_req   <= 1;     // r_req set before read state, so we can expect valid data in READ state
+                        mhp_state   <= MHP_TASK2;
+                    end else begin
+                        r_req   <= 0;
+                    end
+                end
+
+                MHP_TASK2: begin
+                    if (i_rready) // clear fifo
+                        r_req   <= 1;
+                    else begin
+                        r_req   <= 0;
+                        mhp_state   <= MHP_PING;
+                    end
+                end
+
+                MHP_PING: begin
+                    our_scs <= 16'h0000;
+                    our_type <= 7'h01;
+                    our_payload[7:0] <= 8'h00;
+                    our_payload[15:8] <= 8'h00;
+                    our_payload[23:16] <= 8'h00;
+                    i_payload_size <= 37;
+                    if (!frame_assembly_done) begin
+                        frame_assembly_start  <= 1'b1;
+                    end
+                    else begin
+                        mhp_state   <= MHP_ADD_R1;
+                        frame_assembly_start <= 1'b0;
+                        link_delay <= 500;
+                    end
+                end
+
+                MHP_ADD_R1: begin
+                    if (i_rready) begin // received frame's payload ready
+                        r_req   <= 1;     // r_req set before read state, so we can expect valid data in READ state
+                        mhp_state   <= MHP_ADD_R2;
+                    end else begin
+                        r_req   <= 0;
+                    end
+                end
+
+                MHP_ADD_R2: begin
+                    if (i_rready) // clear fifo
+                        r_req   <= 1;
+                    else begin
+                        r_req   <= 0;
+                        mhp_state   <= MHP_ADD_S;
+                    end
+                end
+
+                MHP_ADD_S: begin
+                    our_scs <= 16'h0000;
+                    our_type <= 7'h0E;
+                    our_payload[15:0] <= o_payload[23:8] + o_payload[39:24];
+                    our_payload[23:16] <= 8'h00;
+                    i_payload_size <= 37;
+                    if (!frame_assembly_done) begin
+                        frame_assembly_start  <= 1'b1;
+                    end
+                    else begin
+                        mhp_state   <= MHP_DELAY;
+                        frame_assembly_start <= 1'b0;
+                        link_delay <= 500;
+                    end
+                end
+
+                MHP_DELAY: begin
                     link_delay <= link_delay - 1;
                     if (link_delay == 0) begin
                         link    <= 0;
                         mhp_state   <= MHP_ADDR_REQ;
                     end
-
-                    // if (i_wready) begin
-                    //     w_valid <= 1;
-                    //     mhp_state   <= MHP_READ;
-                    // end
                 end
             endcase
         end
@@ -198,3 +339,5 @@ assign    o_wdata  = link ? o_wdata2  : w_data;
 assign    o_wvalid = link ? o_wvalid2 : w_valid;
 
 endmodule
+
+`default_nettype wire
